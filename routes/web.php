3e65a19c2cfb00c2e39ff6dcf5bcbc985194cdde
samarabdelmageed <samarabdelmageed@yahoo.com>
2023-12-02 11:57:12 +0200

    I deleted a repeated line

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/About',function(){
     return view('About');
 });
